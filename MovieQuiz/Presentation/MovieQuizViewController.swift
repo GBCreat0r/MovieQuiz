ca4221feadab209d95f3feedc847f5298692b7c5
GBC <gbc@semrum.ru>
2025-01-09 12:44:40 +0300

    Правки

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -118,7 +118,8 @@ final class MovieQuizViewController: UIViewController {
                                       preferredStyle: .alert)
         
         let action = UIAlertAction(title: result.buttonText,
-                                   style: .default) {_ in
+                                   style: .default) {[weak self] _ in
+            guard let self else {return}
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             self.show(quiz: self.convert(question: self.questions[self.currentQuestionIndex]))
@@ -133,7 +134,8 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         if isCorrect {correctAnswers += 1}
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self else {return}
             self.imageView.layer.borderWidth = 0
             self.enableAndDisableButtonsSwitcher()
             self.showNextQuestionOrResults()
